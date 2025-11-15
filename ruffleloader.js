dc2f65cf650489385416c45a5e658349c913c901
Simon <133875585+simpansoftware@users.noreply.github.com>
2025-11-15 23:04:15 +0100

    Create ruffleloader.js

---
+++ ruffleloader.js
@@ -0,0 +1,5 @@
+document.addEventListener("keydown", function(event) {
+  if (event.key === "r") {
+    document.open();document.write('<html lang=en><meta charset=UTF-8><link href=./icon32.png rel=icon sizes=32x32><link href=./icon180.png rel=icon sizes=180x180><meta content="width=device-width,initial-scale=1"name=viewport><title>Ruffle Player</title><script crossorigin src=https://cdn.jsdelivr.net/gh/Synaptic-1234/dwaboutit@latest/ruffle/assets/index.js type=module></script><link href=https://cdn.jsdelivr.net/gh/Synaptic-1234/dwaboutit@latest/ruffle/assets/index-BwTJn-HL.css rel=stylesheet crossorigin><div id=root></div>');document.close();
+  }
+});
