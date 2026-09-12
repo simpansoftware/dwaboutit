4079d12898d22e3ab26b0b08a215dc66bc1ead5c
Simon <133875585+simpansoftware@users.noreply.github.com>
2026-09-12 17:36:57 +0200

    Update figo.js

--- figo.js
+++ figo.js
@@ -5,7 +5,7 @@ document.write(`
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>proxy</title>
+    <title>Figo And Friends</title>
     <style>
       html, body { height:100%; margin:0; overflow:hidden; }
       iframe { position:fixed; inset:0; width:100vw; height:100vh; border:0; }
